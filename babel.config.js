fd0d2dffb3be719f3d582ea92badf513b6cdbc5b
Biện Thanh Nhựt <92388024+BThanhNhut@users.noreply.github.com>
2025-02-05 14:12:38 +0700

    feat: config alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@assets': './assets',
+
+          '@common': './src/common',
+          '@library': './src/library',
+          '@model': './src/model',
+          '@navigation': './src/navigation',
+          '@redux': './src/redux',
+          '@screens': './src/screens',
+          '@themes': './src/themes',
+
+          '@redux-slice': './src/redux/action-slice',
+          '@redux-selector': './src/redux/selector',
+          '@redux-action-type': './src/redux/action-type',
+
+          '@listener': './src/common/redux/listener.ts',
+        },
+      },
+    ],
+  ],
 };
