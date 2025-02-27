3510b29fbd907e876098f6561998aeab0609b230
Biện Thanh Nhựt <92388024+BThanhNhut@users.noreply.github.com>
2025-02-27 13:27:04 +0700

    feat: add babel config

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,32 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin',
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@assets': './assets',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      ['react-native-unistyles/plugin'],
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@assets': './assets',
 
-          '@common': './src/common',
-          '@library': './src/library',
-          '@model': './src/model',
-          '@navigation': './src/navigation',
-          '@redux': './src/redux',
-          '@screens': './src/screens',
-          '@themes': './src/themes',
+            '@common': './src/common',
+            '@library': './src/library',
+            '@model': './src/model',
+            '@navigation': './src/navigation',
+            '@redux': './src/redux',
+            '@screens': './src/screens',
+            '@themes': './src/themes',
 
-          '@redux-slice': './src/redux/action-slice',
-          '@redux-selector': './src/redux/selector',
-          '@redux-action-type': './src/redux/action-type',
+            '@redux-slice': './src/redux/action-slice',
+            '@redux-selector': './src/redux/selector',
+            '@redux-action-type': './src/redux/action-type',
 
-          '@listener': './src/common/redux/listener.ts',
+            '@listener': './src/common/redux/listener.ts',
+          },
         },
-      },
+      ],
+      'react-native-reanimated/plugin',
     ],
-  ],
-};
+  };
+}
